ea6b700ba4b05e2bf911194f5c49fd9de05c1547
akim1995 <a.khalitov@clientlab.ru>
2019-10-19 15:54:02 +0200

    ✨ started basic express on port from env file

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,13 @@
-console.log("test");
+import express from "express";
+import dotenv from "dotenv";
+
+// Load env vars
+dotenv.config({ path: "./config/config.env" });
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+app.listen(PORT, () => {
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+  );
+});
